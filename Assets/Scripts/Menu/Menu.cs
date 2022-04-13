1222e7eb0d9ad116a3752a1b8de7c0da18340918
eyeless <maxim.chevychelov@gmail.com>
2022-04-13 23:41:01 +0500

    Добавлены рабочие настройки с разрешением и звуком

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,21 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 
 public class Menu : MonoBehaviour
 {
     public bool isOpened;
-
-    private void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -25,12 +13,21 @@ public class Menu : MonoBehaviour
     
     private void ShowHideMenu()
     {
+        if (GameObject.Find("SettingsCanvas").GetComponent<Canvas>().enabled)
+            return;
+        
         isOpened = !isOpened;
         GetComponent<Canvas>().enabled = isOpened;
     }
     
     public void GoToMain()
     {
-        SceneManager.LoadScene("Main Menu"); //Переход на сцену с названием Menu
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ShowSettings()
+    {
+        GetComponent<Canvas>().enabled = false;
+        GameObject.Find("SettingsCanvas").GetComponent<Canvas>().enabled = true;
     }
 }
---
+++ Assets/Scripts/Menu/Settings.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Работа с интерфейсами
+using UnityEngine.SceneManagement; //Работа со сценами
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public float volume; 
+    public AudioMixer audioMixer; 
+    public Dropdown resolutionDropdown; 
+    private Resolution[] resolutions;
+    private int currResolutionIndex;
+    public Canvas canvas;
+
+    private void Start()
+    {
+        AddResolutions();
+        canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            GoToMain();
+    }
+
+    public void GoToMain()
+    {
+        canvas.enabled = false;
+        GameObject.Find("MenuCanvas").GetComponent<Canvas>().enabled = true;
+    }
+
+    public void ChangeVolume(float val)
+    {
+        volume = val;
+    }
+
+    public void ChangeResolution(int index)
+    {
+        currResolutionIndex = index;
+    }
+    
+    public void SaveSettings()
+    {
+        audioMixer.SetFloat("MasterVolume", volume); 
+        Screen.SetResolution
+            (Screen.resolutions[currResolutionIndex].width, Screen.resolutions[currResolutionIndex].height, true); //Изменения разрешения
+    }
+
+    public void AddResolutions()
+    {
+        resolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions; 
+        List<string> options = new List<string> (); 
+
+        for(var i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions [i].width + " x " + resolutions [i].height;
+            options.Add(option);
+
+            if(resolutions[i].Equals(Screen.currentResolution)) 
+            {
+                currResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options); 
+        resolutionDropdown.value = currResolutionIndex;
+        resolutionDropdown.RefreshShownValue(); 
+    }
+}
