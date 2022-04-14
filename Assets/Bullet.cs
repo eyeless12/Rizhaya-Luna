2907b32bafb67a7c2453b0c5883dc041ffa86cd6
Dyrda Lev <akronman0000@gmail.com>
2022-04-14 18:47:05 +0500

    Столновения пуль и препятствий

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private float _speed = 20f;
+    [SerializeField] private float speed = 20f;
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.right * _speed * -1;
+        rb.velocity = transform.right * speed * -1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.name);
+        Destroy(gameObject);
     }
 }
